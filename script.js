20d40a27f654246c5e146017b72865cb7166b724
huyhiendev <huyhienht2001@gmail.com>
2023-07-07 01:11:50 +0700

    Hoan thanh home page

---
+++ script.js
@@ -0,0 +1,16 @@
+/* Cap nhat clich nut trong mobile */
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
+
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
+
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.remove('active');
+    })
+}
